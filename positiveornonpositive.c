110295644ff9d87e969f3394dc580ce5de4dd473
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-07-27 21:19:23 +0530

    Add files via upload

---
+++ positiveornonpositive.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a number");
+    scanf("%d",&a);
+    if(a>0)
+    printf("positive");
+    if(a<=0)
+    printf("non positive");
+    return 0;
+}
